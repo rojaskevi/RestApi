c2e3a751bd61c7aef55ae65b151354940a8d1d4d
rojaskevi <kevcabanar@gmail.com>
2021-03-24 17:59:23 -0500

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+
+const router = new Router();
+
+router.get('/test', (req, res) => {
+    const data = {
+        name: 'Fazt',
+        website: 'faztweb.com'
+    };
+    res.json(data);
+});  
+
+
+
+module.exports = router;
